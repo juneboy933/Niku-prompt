972b4ee317402963592d962a3b57bb32c61a43fb
juneboy933 <brianoduor03@gmail.com>
2026-08-27 12:26:36 +0300

    Updated invoice and transaction modules and ledger module

--- src/ledger/ledger.module.ts
+++ src/ledger/ledger.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LedgerService } from './ledger.service';
+import { InvoiceModule } from 'src/invoice/invoice.module';
+import { TransactionModule } from 'src/transaction/transaction.module';
 
 @Module({
-  providers: [LedgerService]
+  imports: [InvoiceModule, TransactionModule],
+  providers: [LedgerService],
+  exports: [LedgerService],
 })
 export class LedgerModule {}
--- src/ledger/ledger.service.ts
+++ src/ledger/ledger.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InvoiceService } from 'src/invoice/invoice.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { TransactionService } from 'src/transaction/transaction.service';
 
 @Injectable()
@@ -7,7 +8,30 @@ export class LedgerService {
   constructor(
     private readonly invoice: InvoiceService,
     private readonly transaction: TransactionService,
+    private readonly prisma: PrismaService,
   ) {}
 
-  //   Record settlement
+  // Record settlement
+  async recordSettlement(
+    transactionId: string,
+    mpesaReceipt: string,
+    amount: number,
+  ) {
+    return this.prisma.$transaction(async (tx) => {
+      const transaction = await this.transaction.markAsSuccess(
+        transactionId,
+        tx,
+      );
+      const ledgerEntry = await tx.ledger.create({
+        data: { transactionId: transaction.id, mpesaReceipt, amount },
+      });
+
+      await this.invoice.settleInvoice(transaction.invoiceId, tx);
+
+      return {
+        message: 'Ledger recorded successfully',
+        data: ledgerEntry,
+      };
+    });
+  }
 }
